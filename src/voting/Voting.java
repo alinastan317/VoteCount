3b25b015b626558f7fb7b8596f706306f6ca16fe
Alina <alina.recho@gmail.com>
2018-03-08 18:14:11 +0100

    com

--- src/voting/Voting.java
+++ src/voting/Voting.java
@@ -1,12 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * There are n citizens voting in this year's HackLand election. Each voter writes the name of their chosen candidate 
+ * on a ballot and places it in a ballot box. The candidate with the highest number of votes wins the election; 
+ * if two or more candidates have the same number of votes, then the tied candidates' names are ordered alphabetically 
+ * and the last name wins.
+ * Create an application that accepts the input format and returns a string representing the name of the winning candidate.
+ * 
+ * Input Format
+ * The first line contains an integer, n, denoting the size of the votes array. Each line i of the n subsequent lines (where 0 ≤ i < n) of strings contains a citizen's vote in the form of a candidate's name.
+ * 
+ * Constraints
+ * 1 ≤ n ≤ 104
+ * 
+ * Output Format
+ * Your application must return a string denoting the name of the winner. This should be printed to stdout.
  */
 package voting;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
